c04b67785688e3b668c77d2e5bfd4082ffd9d231
Vijay Kandy <vkandy@gmail.com>
2019-11-09 13:44:55 -0500

    Migration, transfer tokens and ownership of registry to driver contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,27 @@
-const GIFT = artifacts.require("./GIFT.sol");
+const GIFT                = artifacts.require("./GIFT.sol");
+const BeneficiaryRegistry = artifacts.require("./BeneficiaryRegistry.sol");
+const CarbonOffsetProgram = artifacts.require("./CarbonOffsetProgram.sol");
 
 const config = require("../config");
-const fs = require("fs");
-
-console.log(config.get("token:initialSupply"))
-
-let token;
 
 module.exports = function (deployer, network, accounts) {
     deployer.then(async () => {
-            token = await deployer.deploy(GIFT, config.get("token:initialSupply"));
-        })
-        .catch((err) => {
-            console.error("Deployment failed", err);
-        })
+        let gift     = await deployer.deploy(GIFT, config.get("token:initialSupply"));
+        let registry = await deployer.deploy(BeneficiaryRegistry);
+        let driver   = await deployer.deploy(CarbonOffsetProgram, gift.address, registry.address);
+
+        // Transfer all tokens to driver contract
+        let totalSupply = await gift.totalSupply();
+        await gift.transfer(driver.address, totalSupply);
+
+        // Driver controls registry
+        await registry.transferOwnership(driver.address);
+
+        // let balance = await await gift.balanceOf(driver.address);
+        // console.log(totalSupply.toString());
+        // console.log(balance.toString());
+    })
+    .catch((err) => {
+        console.error("Deployment failed", err);
+    })
 };
